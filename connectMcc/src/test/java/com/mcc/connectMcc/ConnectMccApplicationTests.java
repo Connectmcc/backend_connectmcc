1acb3636a46440ad0f3fb2b152bcf4941604dfd4
SINGARAVEL PALANIVEL <connectmcc.dev@gmail.com>
2024-02-10 16:01:22 +0530

    hi

---
+++ connectMcc/src/test/java/com/mcc/connectMcc/ConnectMccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcc.connectMcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectMccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
