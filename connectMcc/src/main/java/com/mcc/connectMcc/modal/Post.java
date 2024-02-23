60f0d3777196978db7275e0b0be24b4a8e453fa1
SINGARAVEL PALANIVEL <connectmcc.dev@gmail.com>
2024-02-23 17:33:13 +0530

    23

--- connectMcc/src/main/java/com/mcc/connectMcc/modal/Post.java
+++ connectMcc/src/main/java/com/mcc/connectMcc/modal/Post.java
@@ -39,7 +39,119 @@ public class Post {
 	
 	@OneToMany
 	private List<Comments> comments=new ArrayList<Comments>();
-    private Set<UserDto>
+    private Set<UserDto> likedByUsers-new HasSet<>{}
+
+
+
+	public Post(Integer id, String caption, String image, String location, LocalDateTime createdAt, UserDto user,
+			List<Comments> comments, Set<UserDto> likedByUsers) {
+		super();
+		this.id = id;
+		this.caption = caption;
+		this.image = image;
+		this.location = location;
+		this.createdAt = createdAt;
+		this.user = user;
+		this.comments = comments;
+		this.likedByUsers = likedByUsers;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public String getCaption() {
+		return caption;
+	}
+
+
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
+
+	public UserDto getUser() {
+		return user;
+	}
+
+
+
+	public void setUser(UserDto user) {
+		this.user = user;
+	}
+
+
+
+	public List<Comments> getComments() {
+		return comments;
+	}
+
+
+
+	public void setComments(List<Comments> comments) {
+		this.comments = comments;
+	}
+
+
+
+	public Set<UserDto> getLikedByUsers() {
+		return likedByUsers;
+	}
+
+
+
+	public void setLikedByUsers(Set<UserDto> likedByUsers) {
+		this.likedByUsers = likedByUsers;
+	};
+    
 	
 	
 }
