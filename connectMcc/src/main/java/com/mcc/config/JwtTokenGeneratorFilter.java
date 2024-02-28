f0ebb42f8ce2bb934ec45cfb3cc1b47f93e4d9ae
Connectmcc <connectmcc.dev@gmail.com>
2024-02-28 17:03:15 +0530

    at 17,53

--- connectMcc/src/main/java/com/mcc/config/JwtTokenGeneratorFilter.java
+++ connectMcc/src/main/java/com/mcc/config/JwtTokenGeneratorFilter.java
@@ -1,6 +1,7 @@
 package com.mcc.config;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.crypto.SecretKey;
 
@@ -8,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -24,7 +26,16 @@ public class JwtTokenGeneratorFilter extends OncePerRequestFilter {
        		
 		if(authentication!=null) {
 			SecretKey key=Keys.hmacShaKeyFor(SecurityContext.JWT_KEY.getBytes());
+			
+			String jwt=Jwts.builder()
+					.setIssuer("instagram")
+					.setIssuedAt(new Date())
+					.claim("authorities",populateAuthorities(authentication.getAuthorities()))
+					.claim("username",authentication.getName())
+					
 		}
 	}
-
+	public String populateAuthorities()
+	}
+	}
 }
