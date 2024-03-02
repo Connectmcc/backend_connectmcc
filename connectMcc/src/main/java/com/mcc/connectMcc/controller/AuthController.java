1ce91041e6b9d1b471f336630813837040abbb5a
Connectmcc <connectmcc.dev@gmail.com>
2024-03-02 14:30:40 +0530

    52

--- connectMcc/src/main/java/com/mcc/connectMcc/controller/AuthController.java
+++ connectMcc/src/main/java/com/mcc/connectMcc/controller/AuthController.java
@@ -1,8 +1,12 @@
 package com.mcc.connectMcc.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +23,16 @@ public class AuthController {
 	public ResponseEntity<User> registerUserHandler(@RequestBody User user) throws UserException{
 		User createdUser=userService.registerUser(user);
 		return new ResponseEntity<User>(createdUser,HttpStatus.OK);
+	}
+	
+	public ResponseEntity<User> signinHandler(Authentication auth) throws BadCredentialsException{
+		
+		Optional<User> opt=userRepo.findByEmail(auth.getName());
+		if(opt.isPresent()) {
+			
+		}
+		
+		
+		return null;
 	}
 }
